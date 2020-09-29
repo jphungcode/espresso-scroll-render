163bb6a1a72f1c139110535c6a33d779dc7237e4
John Phung <john.phung101@hotmail.com>
2020-09-29 17:18:16 +1000

    new commit

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   const intro = document.querySelector(".intro");
 
-  const text = intro.querySelector("h1");
-
-  // const section = document.querySelector("section");
-
-  // const end = document.querySelector("h1");
-
   const controller = new ScrollMagic.Controller();
+  const elem = document.getElementById("lottie");
+  let anim;
 
   // set desired animation time
-  const animationTime = 809;
+  // 809 frames - 3x
+  const animationTime = 2427;
 
   let scene = new ScrollMagic.Scene({
     duration: animationTime,
@@ -20,20 +17,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     .setPin(intro)
     .addTo(controller);
 
-  let textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
-
-  // animation for text
-  let scene2 = new ScrollMagic.Scene({
-    duration: 200,
-    triggerElement: intro,
-    triggerHook: 0,
-  })
-    .setTween(textAnim)
-    .addTo(controller);
-
   // Video animation
-  // let accelamount = 0.1;
-  // let scrollPos = 0;
   let delay = 0;
   let heightPerFrame = 0;
 
@@ -42,9 +26,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
     delay = Math.round(e.scrollPos * heightPerFrame);
     anim.goToAndStop(delay, true);
   });
-
-  let anim;
-  let elem = document.getElementById("lottie");
 
   const animateData = {
     container: elem, //
